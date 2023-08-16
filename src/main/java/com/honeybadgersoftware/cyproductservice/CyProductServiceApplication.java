6f65da4c2a8e95c30bfb57427d11ad0efc4b24ad
Jakub <jakub.kolacz.dev@gmail.com>
2023-08-16 21:53:01 +0200

    initial commit

---
+++ src/main/java/com/honeybadgersoftware/cyproductservice/CyProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.honeybadgersoftware.cyproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyProductServiceApplication.class, args);
+	}
+
+}
